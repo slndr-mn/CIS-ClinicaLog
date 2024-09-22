f9eb217f909db42705f00ffc169466f8d5b16a3d
qwyqwyn <casiagwynethmarie@gmail.com>
2024-09-22 20:22:19 +0800

    php client

---
+++ php-client/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Sample Index</title> 
+    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" /> 
+    <link rel="icon" href="../assets/img/ClinicaLog.ico" type="image/x-icon"/>
+
+    <!-- Fonts and icons -->
+    <script src="../assets/js/plugin/webfont/webfont.min.js"></script>
+    <script>
+      WebFont.load({ 
+        google: { families: ["Public Sans:300,400,500,600,700"] },
+        custom: {
+          families: [ 
+            "Font Awesome 5 Solid",
+            "Font Awesome 5 Regular",
+            "Font Awesome 5 Brands",
+            "simple-line-icons",
+          ],
+          urls: ["../css/fonts.min.css"], 
+        },
+        active: function () {
+            
+          sessionStorage.fonts = true;
+        },
+      });
+    </script>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/plugins.min.css" />
+    <link rel="stylesheet" href="../css/kaiadmin.min.css" />
+
+    <!-- ICONS -->
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+    <style>
+      .sidebar {
+          transition: background 0.3s ease;
+          /* Initial background */
+          background: linear-gradient(to bottom, #DB6079, #DA6F65, #E29AB4);
+      }
+      .logo-header {
+          transition: background 0.3s ease;
+      }
+      .nav-item.active {
+            background-color: rgba(0, 0, 0, 0.1); 
+            color: #fff; 
+        }
+
+        .nav-item.active i {
+            color: #fff;
+        }
+
+  </style>
+</head>
+<body>
+    <div class="wrapper">
+                <h1>
+                Client Side
+                </h1>
+    </div>
+
+    
+    
+
+    <script src="../assets/js/core/jquery-3.7.1.min.js"></script>
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+
+    <!-- jQuery Scrollbar -->
+    <script src="../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+ 
+    <!-- Chart JS -->
+    <script src="../assets/js/plugin/chart.js/chart.min.js"></script>
+
+    <!-- jQuery Sparkline -->
+    <script src="../assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+
+    <!-- Chart Circle -->
+    <script src="../assets/js/plugin/chart-circle/circles.min.js"></script>
+
+    <!-- Datatables -->
+    <script src="../assets/js/plugin/datatables/datatables.min.js"></script>
+
+    <!-- Bootstrap Notify -->
+    <script src="../assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+
+    <!-- jQuery Vector Maps -->
+    <script src="../assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
+    <script src="../assets/js/plugin/jsvectormap/world.js"></script>
+
+    <!-- Sweet Alert -->
+    <script src="../assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+
+    <!-- Kaiadmin JS -->
+    <script src="../assets/js/kaiadmin.min.js"></script>
+
+</body>
+</html>
